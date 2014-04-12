5aacc75b7e64c960214f8874db9315a63e355239
Mateusz Machalica <mateuszmachalica@gmail.com>
2014-04-12 15:11:48 +0200

    Performance testing script.

---
+++ perftest.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+LOG_FILE=${LOG_FILE:-"$HOME/perftest.log"}
+OUT_FILE=${OUT_FILE:-"$HOME/perftest.out"}
+BRANDES=${BRANDES:-"./brandes"}
+
+touch "$LOG_FILE"
+exec >  >(tee -a "$LOG_FILE")
+exec 2> >(tee -a "$LOG_FILE" >&2)
+
+echo "TEST BEGIN | `date` | $# graphs"
+for graph in "$@"; do
+  echo -ne "`basename $graph` \t "
+  ts=$(date +%s%N)
+  $BRANDES $graph $OUT_FILE &>/dev/null
+  te=$(date +%s%N)
+  echo "$((($te-$ts)/1000000))"
+done
+echo "TEST END"
+
